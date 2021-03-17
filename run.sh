e49e96497c2d001bdb5e350ff3dc663584d26a2a
JlXip <jlxip@protonmail.com>
2021-03-17 21:33:53 +0100

    Add files via upload

--- run.sh
+++ run.sh
@@ -6,7 +6,9 @@ mkdir -p $CONFPATH
 cp /benchmarks.json /root/.config/nuxhash/
 cp /settings.conf /root/.config/nuxhash/
 sed -i "s/TheMiner/$HOSTNAME/g" /root/.config/nuxhash/settings.conf
-nuxhashd &; disown
+
+nuxhashd &
+disown
 
 mkdir -p /var/www
 cd /var/www
